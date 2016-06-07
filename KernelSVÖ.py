5624c73944efd6d70dd0f9fcbb309978fa60b3eb
lh <louis.franc@insa-rouen.fr>
2016-06-07 16:35:09 -0400

    Gaussian Kernel

---
+++ KernelSVÖ.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.svm import SVC
+
+np.random.seed(0)
+x_xor = np.random.randn(200, 2)
+
+y_xor = np.logical_xor(x_xor[:, 0] > 0, x_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+plt.scatter(x_xor[y_xor == 1, 0],
+            x_xor[y_xor == 1, 1],
+            c='b', marker='x',
+            label='1')
+plt.scatter(x_xor[y_xor == -1, 0],
+            x_xor[y_xor == -1, 1],
+            c='r',
+            marker='s',
+            label='-1')
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(x_xor, y_xor)
+from ScikitLearn import plot_decision_regions
+
+plot_decision_regions(x_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
