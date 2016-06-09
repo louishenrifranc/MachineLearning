c23c266956d33d1ea410a98812c1067c5375cf24
lh <louis.franc@insa-rouen.fr>
2016-06-09 09:47:57 -0400

    LDA finit

--- LinearDiscriminantAnalysis.py
+++ LinearDiscriminantAnalysis.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from matplotlib.colors import ListedColormap
 from sklearn.cross_validation import train_test_split
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+
+from ScikitLearn import plot_decision_regions
 
 df_wine = pd.read_csv('https://archive.ics.uci.edu/'
                       'ml/machine-learning-databases/wine/wine.data',
@@ -18,42 +18,25 @@ df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
 X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
 
 X_train, X_test, y_train, y_test = \
-    train_test_split(X, y, test_size=0.3, random_state=0)
+    train_test_split(X, y, test_size=0.3)
 
 # 1 Standardiser
 ss = StandardScaler()
 X_train_std = ss.fit_transform(X_train)
 X_test_std = ss.fit_transform(X_test)
+
+# Scikit LDA, ressortir le vecteur des deux features les plus importantes
 lda = LinearDiscriminantAnalysis(n_components=2)
 X_train_lda = lda.fit_transform(X_train_std, y_train)
 
+# Effectue la logistic Regression
 lr = LogisticRegression()
 lr = lr.fit(X_train_lda, y_train)
 
-
-def plot_decision_regions(X, y, classifier, resolution=0.02):
-    # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    # plot class samples
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
-                    alpha=0.8, c=cmap(idx),
-                    marker=markers[idx], label=cl)
-
-
-plot_decision_regions(X_train_lda, y_train, lr)
+# On transforme nos données de test (il ne faut pas fitter a nouveau, ici on transforme X_test_std en recuperant uniquement les features qui ont été choisies lors
+# de l'entrainement
+X_test_lda = lda.transform(X_test_std)
+# plot_decision_regions(X_train_lda, y_train, lr)
+#
+plot_decision_regions(X_test_lda, y_test, lr)
 plt.show()
