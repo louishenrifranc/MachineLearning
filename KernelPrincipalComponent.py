d8d4103d3941234f13e280d993424fcd289261fb
lh <louis.franc@insa-rouen.fr>
2016-06-09 10:12:46 -0400

    KernelPC

---
+++ KernelPrincipalComponent.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+df_wine = pd.read_csv('https://archive.ics.uci.edu/'
+                      'ml/machine-learning-databases/wine/wine.data',
+                      header=None)
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
+                   'Color intensity', 'Hue', 'OD280/OD315 of diluted wines',
+                   'Proline']
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+
+X_train, X_test, y_train, y_test = \
+    train_test_split(X, y, test_size=0.3)
+
+# 1 Standardiser
+ss = StandardScaler()
+X_train_std = ss.fit_transform(X_train)
+X_test_std = ss.fit_transform(X_test)
+# poUR PLUS TARD LE RESTE
+
+X, y = make_moons(n_samples=100, random_state=123)
+scikit_kpca = KernelPCA(n_components=2, kernel='rbf', gamma=15)
+X_skernpca = scikit_kpca.fit_transform(X)
+
+plt.scatter(X_skernpca[y == 0, 0], X_skernpca[y == 0, 1],
+            color='red', marker='^', alpha=0.5)
+plt.scatter(X_skernpca[y == 1, 0], X_skernpca[y == 1, 1],
+            color='blue', marker='o', alpha=0.5)
+
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+# plt.tight_layout()
+# plt.savefig('./figures/scikit_kpca.png', dpi=300)
+plt.show()
