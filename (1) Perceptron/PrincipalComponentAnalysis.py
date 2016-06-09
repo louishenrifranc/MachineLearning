a75cbb18488f60abe426971c1a475363b79dfc02
lh <louis.franc@insa-rouen.fr>
2016-06-09 08:38:12 -0400

    PCA 1

--- (1) Perceptron/PrincipalComponentAnalysis.py
+++ (1) Perceptron/PrincipalComponentAnalysis.py
@@ -1,6 +1,10 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from matplotlib.colors import ListedColormap
 from sklearn.cross_validation import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
 df_wine = pd.read_csv('https://archive.ics.uci.edu/'
@@ -33,4 +37,42 @@ paire_propre = [(np.abs(valpropre[i]), vecpropre[:, i])
 paire_propre.sort(reverse=True)
 
 # 5. Former la matrice de transition en prenant ici deux features
-np.hstack(paire_propre[0][1][:, np.newaxis], paire_propre[1, 1][:, np.newaxis])
+W = np.hstack((paire_propre[0][1][:, np.newaxis], paire_propre[1][1][:, np.newaxis]))
+
+# 6. Passer dans la nouvelle base
+X_train_nbase = X_train_std.dot(W)
+
+############################################################################
+# On peut utiliser la librairie Skicitlearn
+
+pca = PCA(n_components=2)
+lr = LogisticRegression()
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+lr.fit(X_train_pca, y_train)
+
+
+# Ploter
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
