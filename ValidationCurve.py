556aac6f5e6174530e8d0b55439fcdec835ee6c9
lh <louis.franc@insa-rouen.fr>
2016-06-10 14:51:21 -0400

    ValidationCurve

--- ValidationCurve.py
+++ ValidationCurve.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.cross_validation import train_test_split
+from sklearn.learning_curve import validation_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
@@ -23,3 +26,38 @@ X_test_std = sc.fit_transform(X_test)
 
 pipe_lr = Pipeline([('scl', StandardScaler()),
                     ('clf', LogisticRegression(penalty='l2', random_state=0))])
+
+param_range = [0.1, 1, 10, 50, 100, 1000]
+train_scores, test_scores = validation_curve(estimator=pipe_lr, X=X_train, y=y_train, cv=10, param_range=param_range,
+                                             param_name='clf__C', n_jobs=1)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(param_range, train_mean,
+         color='blue', marker='o',
+         markersize=5, label='training accuracy')
+
+plt.fill_between(param_range, train_mean + train_std,
+                 train_mean - train_std, alpha=0.15,
+                 color='blue')
+
+plt.plot(param_range, test_mean,
+         color='green', linestyle='--',
+         marker='s', markersize=5,
+         label='validation accuracy')
+
+plt.fill_between(param_range,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha=0.15, color='green')
+
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+plt.show()
