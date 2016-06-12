12f2919c8ebb4462d55dfee1134bb8832cc3ffa0
lh <louis.franc@insa-rouen.fr>
2016-06-12 17:49:20 -0400

    Linear Regression optimization 2

--- LinearRegression/DimensionnalPolynomialRegression.py
+++ LinearRegression/DimensionnalPolynomialRegression.py
@@ -19,37 +19,49 @@ df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS',
 X = df[['LSTAT']].values
 y = df['MEDV'].values
 
+
 regr = LinearRegression()
 
-# Implémenté dans la bibliothéque
+# Pour augmenter le nombre de dimensions, il suffit de modifier les données initiales pour notre regression, en
+# éffectuant des combinaisons de features. On le passe ensuite a notre regression pour l'entrainement.
+# A noter qu'il faut modifier la dimension des données d'entrainement mais aussi des données de validation !
 
 # Génère une nouvelle matrice contenant toutes les combinaisons de features de degré inférieure ou égale à 2
 quadratic = PolynomialFeatures(degree=2)
-
 cubic = PolynomialFeatures(degree=3)
 X_quad = quadratic.fit_transform(X)
 X_cubic = cubic.fit_transform(X)
 
 # Fit les différentes régréssions
+# 1 D
 X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
 regr = regr.fit(X, y)
 y_lin_fit = regr.predict(X_fit)
 linear_r2 = r2_score(y, regr.predict(X))
+
+# 2 D
 regr = regr.fit(X_quad, y)
 y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
 quadratic_r2 = r2_score(y, regr.predict(X_quad))
 
+# 3 D
 regr = regr.fit(X_cubic, y)
 y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
 cubic_r2 = r2_score(y, regr.predict(X_cubic))
 
-# plot results
+# On affiche nos points et les courbes obtenues
+
+# Pour afficher des points
 plt.scatter(X, y, label='training points', color='lightgray')
+
+# Pour afficher une courbe de Y en fonction de X
 plt.plot(X_fit, y_lin_fit,
-         label='linear (d=1), $R^2=%.2f$' % linear_r2,
-         color='blue',
-         lw=2,
-         linestyle=':')
+         label='linear (d=1), $R^2=%.2f$' % linear_r2,  # les informations du graphes
+         color='blue',  # couleur
+         lw=2,  # largeur du point
+         linestyle=':')  # Comment on représente le point
+
+# Pour afficher
 plt.plot(X_fit, y_quad_fit,
          label='quadratic (d=2), $R^2=%.2f$' % quadratic_r2,
          color='red',
